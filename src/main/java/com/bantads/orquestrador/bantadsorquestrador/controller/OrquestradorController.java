aad70f51228177a38388cd7670a48e8a385befe1
Raphael Rodrigues <raphael.rodrigues@wisereducacao.com>
2022-08-26 00:48:37 -0300

    refactor

--- src/main/java/com/bantads/orquestrador/bantadsorquestrador/controller/OrquestradorController.java
+++ src/main/java/com/bantads/orquestrador/bantadsorquestrador/controller/OrquestradorController.java
@@ -12,27 +12,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bantads.orquestrador.bantadsorquestrador.DTOs.ClienteDTO;
+import com.bantads.orquestrador.bantadsorquestrador.DTOs.UsuarioDTO;
 import com.bantads.orquestrador.bantadsorquestrador.model.autenticacao.Usuario;
 import com.bantads.orquestrador.bantadsorquestrador.model.cliente.Cliente;
 import com.bantads.orquestrador.bantadsorquestrador.validator.AutocadastroValidator;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/orquestrador")
+@RequestMapping("/autocadastro")
 public class OrquestradorController {
     @Autowired
     private ModelMapper mapper;
 
-    @PostMapping("/autocadastro")
+    @PostMapping
     ResponseEntity<Cliente> cadastro(@RequestBody ClienteDTO clienteDTO) {
         try {
             if (AutocadastroValidator.validate(clienteDTO)) {
-                Cliente cliente = mapper.map(clienteDTO, Cliente.class);
-                Usuario usuario = mapper.map(clienteDTO.getUsuario(), Usuario.class);
-        
-                cliente.setId(UUID.randomUUID());
-                usuario.setId(UUID.randomUUID());
-        
+                UUID saga = UUID.randomUUID();
+                Cliente cliente = mapCliente(clienteDTO, saga);
+                Usuario usuario = mapUsuario(clienteDTO.getUsuario(), saga);
+
+                
                 return ResponseEntity.ok().build();
             } else {
                 return ResponseEntity.badRequest().build();
@@ -40,5 +40,19 @@ public class OrquestradorController {
         } catch(Exception e) {
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    private Cliente mapCliente(ClienteDTO clienteDTO, UUID saga) {
+        Cliente cliente = mapper.map(clienteDTO, Cliente.class);
+        cliente.setId(UUID.randomUUID());
+        cliente.setSaga(saga);
+        return cliente;
+    }
+
+    private Usuario mapUsuario(UsuarioDTO usuarioDTO, UUID saga) {
+        Usuario usuario = mapper.map(usuarioDTO, Usuario.class);
+        usuario.setId(UUID.randomUUID());
+        usuario.setSaga(saga);
+        return usuario;
     }
 }
