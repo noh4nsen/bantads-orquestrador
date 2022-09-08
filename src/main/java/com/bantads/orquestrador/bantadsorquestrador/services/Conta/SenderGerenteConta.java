954c2d82000b9968f98444f7ff9d23479464165f
noh4nsen <noh4nsen@gmail.com>
2022-09-07 21:54:13 -0300

    fix: inclusao de GerenteContaDTO

--- src/main/java/com/bantads/orquestrador/bantadsorquestrador/services/Conta/SenderGerenteConta.java
+++ src/main/java/com/bantads/orquestrador/bantadsorquestrador/services/Conta/SenderGerenteConta.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.bantads.orquestrador.bantadsorquestrador.DTOs.GerenteContaDTO;
 import com.bantads.orquestrador.bantadsorquestrador.model.gerente.Gerente;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +22,8 @@ public class SenderGerenteConta {
     private ObjectMapper objectMapper;
 
     public void send(Gerente gerente) throws JsonProcessingException {
-        String json = objectMapper.writeValueAsString(gerente.getId());
+        GerenteContaDTO gerenteContaDTO = new GerenteContaDTO(gerente.getId(), gerente.getSaga());
+        String json = objectMapper.writeValueAsString(gerenteContaDTO);
         this.template.convertAndSend(this.queueGerenteConta.getName(), json);
     }
 }
